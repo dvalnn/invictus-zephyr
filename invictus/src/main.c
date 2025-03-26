57a52a2289564db45dcda20120e9bd01d0431f84
dvalinn <tiago.andre.amorim@gmail.com>
2025-03-26 12:46:26 +0000

    kernel hello world

--- invictus/src/main.c
+++ invictus/src/main.c
@@ -4,10 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
+#include <zephyr/kernel.h>
 
 int main(void) {
-  printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+  printk("Hello World! %s\n", CONFIG_BOARD_TARGET);
 
   return 0;
 }
