1db8ab8d844864b0361750e4be94303de471598b
dvalinn <tiago.andre.amorim@gmail.com>
2025-08-02 20:12:27 +0100

    Minor tweaks and code improvements.

--- invictus2/obc/include/modbus_thrd.h
+++ invictus2/obc/include/modbus_thrd.h
@@ -9,6 +9,6 @@ struct modbus_data_queues {
 };
 
 bool modbus_thread_setup(void);
-void modbus_thread_entry(void *fsm_config, void *data_queues, void *p3);
+void modbus_thread(void *fsm_config, void *data_queues, void *p3);
 
 #endif
