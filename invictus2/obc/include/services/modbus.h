8d4007cc2a96505924a7b50a5778978133976410
dvalinn <tiago.andre.amorim@gmail.com>
2025-08-10 22:16:04 +0100

    PR #37 code review changes and other minor tweaks.
    - changed new Kconfig options to adhere to the previous naming
    convention.
    - moved duplicated code from HYDRA and LIFT definitions into
    services/modbus.
    - formatting.

--- invictus2/obc/include/services/modbus.h
+++ invictus2/obc/include/services/modbus.h
@@ -1,9 +1,18 @@
 #ifndef MODBUS_THRD_H_
 #define MODBUS_THRD_H_
 
-#include "stdbool.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+struct modbus_slave_metadata {
+    uint8_t slave_id;     // Modbus slave ID
+    uint8_t is_connected; // Connection status
+    uint16_t ir_start;    // Start address for input registers
+};
 
 bool modbus_service_setup(void);
 void modbus_service_start(void);
 
+void modbus_slave_check_connection(const int rc, struct modbus_slave_metadata *const meta,
+                                   const char *const label);
 #endif
--- invictus2/obc/src/lift.c
+++ invictus2/obc/src/lift.c
@@ -1,67 +1,36 @@
 #include "lift.h"
 
+#include "services/modbus.h"
+
 #include "zephyr/kernel.h"
 #include "zephyr/modbus/modbus.h"
 #include "zephyr/logging/log.h"
 
 LOG_MODULE_REGISTER(lift, LOG_LEVEL_INF);
 
-inline void rocket_lift_init(struct r_lift *l)
+inline void lift_init(struct rocket_lift *r, struct fs_lift *l)
 {
-    BUILD_ASSERT((CONFIG_LIFT_R_SLAVE_ID > 0) && (CONFIG_LIFT_FS_SLAVE_ID > 0) &&
-                     (CONFIG_LIFT_R_SLAVE_ID != CONFIG_LIFT_FS_SLAVE_ID),
+    BUILD_ASSERT((CONFIG_MODBUS_ROCKET_LIFT_SLAVE_ID > 0) &&
+                     (CONFIG_MODBUS_FS_LIFT_SLAVE_ID > 0) &&
+                     (CONFIG_MODBUS_ROCKET_LIFT_SLAVE_ID != CONFIG_MODBUS_FS_LIFT_SLAVE_ID),
                  "R and FS lifts must have different, non-zero, slave IDs.");
 
-    if (!l) {
+    if (!r || !l) {
         LOG_ERR("Lift structure pointer is NULL.");
         return;
     }
 
-    memset(l, 0, sizeof(struct r_lift)); // REVIEW: if *h is always statically
-                                            // allocated, this is not needed
-    l->meta.slave_id = CONFIG_LIFT_R_SLAVE_ID;
-    l->meta.ir_start = CONFIG_LIFT_R_INPUT_ADDR_START;
+    memset(r, 0, sizeof(struct rocket_lift)); // REVIEW: if *r is always statically
+    memset(l, 0, sizeof(struct rocket_lift)); // REVIEW: if *l is always statically
+
+    // allocated, this is not needed
+    r->meta.slave_id = CONFIG_MODBUS_ROCKET_LIFT_SLAVE_ID;
+    r->meta.ir_start = CONFIG_MODBUS_ROCKET_LIFT_INPUT_ADDR_START;
+    l->meta.slave_id = CONFIG_MODBUS_FS_LIFT_SLAVE_ID;
+    l->meta.ir_start = CONFIG_MODBUS_FS_LIFT_INPUT_ADDR_START;
 }
 
-inline void fs_lift_init(struct fs_lift *l)
-{
-    BUILD_ASSERT((CONFIG_LIFT_R_SLAVE_ID > 0) && (CONFIG_LIFT_FS_SLAVE_ID > 0) &&
-                 (CONFIG_LIFT_R_SLAVE_ID != CONFIG_LIFT_FS_SLAVE_ID),
-             "R and FS lifts must have different, non-zero, slave IDs.");
-
-    if (!l) {
-        LOG_ERR("Filling Station Lift structure pointer is NULL.");
-        return;
-    }
-
-    memset(l, 0, sizeof(struct fs_lift)); // REVIEW: if *h is always statically
-                                            // allocated, this is not needed
-    l->meta.slave_id = CONFIG_LIFT_FS_SLAVE_ID;
-    l->meta.ir_start = CONFIG_LIFT_FS_INPUT_ADDR_START;
-}
-
-static inline void check_connection(const int rc, struct lift_metadata *meta,
-                                    const char *const label)
-{
-    if (!meta || !label) {
-        LOG_ERR("Invalid parameters for connection check.");
-        return;
-    }
-
-    if (rc < 0 && !meta->is_connected) {
-        return; // Already disconnected, no need to log again
-    }
-
-    if (rc < 0 && meta->is_connected) {
-        LOG_ERR("Failed to read [%s]: %d. Flagging disconnect.", label, rc);
-        meta->is_connected = false;
-    } else if (rc >= 0 && !meta->is_connected) {
-        LOG_INF("Reconnected to [%s].", label);
-        meta->is_connected = true;
-    }
-}
-
-void rocket_lift_sensor_read(const int client_iface, struct r_lift *const l)
+void rocket_lift_sensor_read(const int client_iface, struct rocket_lift *const l)
 {
     if (!l || client_iface < 0) {
         LOG_ERR("Invalid parameters for LIFT sensor read.");
@@ -69,14 +38,14 @@ void rocket_lift_sensor_read(const int client_iface, struct r_lift *const l)
     }
 
     // Read upper feed lift sensors
-    int lift_rc = modbus_read_input_regs(client_iface, l->meta.slave_id, l->meta.ir_start,
-                                       (uint16_t *const)&l->loadcells.raw,
-                                       ARRAY_SIZE(l->loadcells.raw));
+    int read_res = modbus_read_input_regs(client_iface, l->meta.slave_id, l->meta.ir_start,
+                                          (uint16_t *const)&l->loadcells.raw,
+                                          ARRAY_SIZE(l->loadcells.raw));
 
-    check_connection(lift_rc, &l->meta, "Lift sensors");
+    modbus_slave_check_connection(read_res, &l->meta, "Rocket LIFT");
 }
 
-void rocket_lift_coils_read(const int client_iface, struct r_lift *const l)
+void rocket_lift_coils_read(const int client_iface, struct rocket_lift *const l)
 {
     k_oops(); // FIXME: implement this function
 }
@@ -89,11 +58,11 @@ void fs_lift_sensor_read(const int client_iface, struct fs_lift *const l)
     }
 
     // Read filling station lift sensors
-    int lift_rc = modbus_read_input_regs(client_iface, l->meta.slave_id, l->meta.ir_start,
-                                       (uint16_t *const)&l->n2o_loadcell,
-                                       1); // Only one loadcell
+    int read_res = modbus_read_input_regs(client_iface, l->meta.slave_id, l->meta.ir_start,
+                                          (uint16_t *const)&l->n2o_loadcell,
+                                          1); // Only one loadcell
 
-    check_connection(lift_rc, &l->meta, "FS Lift sensors");
+    modbus_slave_check_connection(read_res, &l->meta, "FS LIFT");
 }
 
 void fs_lift_coils_read(const int client_iface, struct fs_lift *const l)
