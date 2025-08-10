7637fa8091321989f21e8b6ccee9b05859a1f9b0
dvalinn <tiago.andre.amorim@gmail.com>
2025-08-10 12:47:37 +0100

    Implemented core review suggestions.
    - Removed all usage of attribute(((packed))).
    - Added error enum for pack / unpack errors.
    - Moved command size BUILD_ASSERT into the MAKE_COMMAND macro.
    - Other minor improvements.

--- invictus2/obc/src/radio_commands.c
+++ invictus2/obc/src/radio_commands.c
@@ -1,42 +1,41 @@
 #include "radio_commands.h"
 
 #include <string.h>
-#include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(radio_commands, LOG_LEVEL_DBG);
+#define CHECK_ARGS(cmd, buf, buf_size)                                                        \
+    do {                                                                                      \
+        if (!cmd || !buf) {                                                                   \
+            return PACK_ERROR_INVALID_ARG;                                                    \
+        }                                                                                     \
+        if (buf_size < RADIO_CMD_SIZE) {                                                      \
+            return PACK_ERROR_BUFFER_TOO_SMALL;                                               \
+        }                                                                                     \
+    } while (0)
 
-int radio_cmd_pack(const struct radio_generic_cmd_s *const cmd, uint8_t *const out_buf,
-                   const size_t out_buf_size)
+#define CHECK_CMD(cmd)                                                                        \
+    do {                                                                                      \
+        if (cmd->header.packet_version != SUPPORTED_PACKET_VERSION) {                         \
+            return PACK_ERROR_UNSUPPORTED_VERSION;                                            \
+        }                                                                                     \
+        if (cmd->header.command_id <= RCMD_NONE || cmd->header.command_id >= RCMD_MAX) {      \
+            return PACK_ERROR_INVALID_CMD_ID;                                                 \
+        }                                                                                     \
+    } while (0)
+
+enum pack_error_e radio_cmd_pack(const struct radio_generic_cmd_s *const cmd,
+                                 uint8_t *const out_buf, const size_t out_buf_size)
 {
-    if (!cmd || !out_buf || out_buf_size < RADIO_CMD_SIZE) {
-        return -1; // Error: Invalid parameters
-    }
-
+    CHECK_ARGS(cmd, out_buf, out_buf_size);
+    CHECK_CMD(cmd);
     memcpy(out_buf, cmd, RADIO_CMD_SIZE);
-    return 0;
+    return PACK_ERROR_NONE;
 }
 
-int radio_cmd_unpack(const uint8_t *const in_buf, const size_t in_buf_size,
-                     struct radio_generic_cmd_s *const cmd)
+enum pack_error_e radio_cmd_unpack(const uint8_t *const in_buf, const size_t in_buf_size,
+                                   struct radio_generic_cmd_s *const cmd)
 {
-    if (!in_buf || !cmd || in_buf_size < RADIO_CMD_SIZE) {
-        LOG_ERR("Invalid parameters for unpack");
-        return -1; // Error: Invalid parameters
-    }
-
-    // unpack the entire command at once
+    CHECK_ARGS(cmd, in_buf, in_buf_size);
     memcpy(cmd, in_buf, RADIO_CMD_SIZE);
-
-    // validate the command header
-    if (cmd->header.packet_version != SUPPORTED_PACKET_VERSION) {
-        LOG_ERR("Invalid packet version: %d", cmd->header.packet_version);
-        return -1; // Error: Invalid packet version
-    }
-
-    if (cmd->header.command_id <= RCMD_NONE || cmd->header.command_id >= RCMD_MAX) {
-        LOG_ERR("Invalid command ID: %d", cmd->header.command_id);
-        return -1; // Error: Invalid command ID
-    }
-
-    return 0;
+    CHECK_CMD(cmd);
+    return PACK_ERROR_NONE;
 }
