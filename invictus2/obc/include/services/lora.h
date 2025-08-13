185f682b70c59449572cbc9d2ee27c4fa7a82585
Tiago Sá Marques <samarques.tiago@gmail.com>
2025-08-13 20:31:15 +0100

    lora: Prepare lora service for testing

--- invictus2/obc/include/services/lora.h
+++ invictus2/obc/include/services/lora.h
@@ -2,6 +2,15 @@
 #define LORA_THRD_H_
 
 #include "stdbool.h"
+#include <assert.h>
+
+// Set to use production backend
+#define CONFIG_LORA_BACKEND 1
+// Set to use backend for testing
+#define CONFIG_LORA_REDIRECT_UART 0
+
+bool lora_setup(void);
+void lora_backend(void);
 
 bool lora_service_setup(void);
 void lora_service_start(void);
