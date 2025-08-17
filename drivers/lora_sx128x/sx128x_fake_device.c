39708765a90a4b98ed3b7be246fd5cf3edf1b62d
Tiago Sá Marques <samarques.tiago@gmail.com>
2025-08-17 14:14:06 +0100

    Add fake sx1280x device driver

---
+++ drivers/lora_sx128x/sx128x_fake_device.c
@@ -0,0 +1,49 @@
+#include <assert.h>
+#include <stdint.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(sx128x_device, CONFIG_LORA_SX128X_LOG_LEVEL);
+
+#include <invictus2/drivers/sx128x.h>
+#include <invictus2/drivers/sx128x_context.h>
+#include <invictus2/drivers/sx128x_hal.h>
+#include <zephyr/drivers/lora.h>
+
+#define DT_DRV_COMPAT zephyr_fake_sx128x
+
+static struct sx1280_data {
+    const struct device *dev;
+    void (*rx_callback)(uint8_t *, uint16_t);
+} dev_data;
+
+static int sx128x_init(const struct device *dev)
+{
+    LOG_INF("fake_sx128x: init");
+    dev_data.dev = dev;
+    return SX128X_STATUS_OK;
+}
+
+void sx1280x_write(uint8_t *data, size_t size)
+{
+}
+
+void sx128x_register_recv_callback(void (*rx_callback)(uint8_t *, uint16_t))
+{
+    dev_data.rx_callback = rx_callback;
+}
+
+#define SX128X_DEFINE(inst)                                                                   \
+    static struct sx128x_context_data sx128x_data_##node_id;                                  \
+                                                                                              \
+    static const struct sx128x_context_cfg sx128x_config_##node_id = {};                      \
+                                                                                              \
+    DEVICE_DT_INST_DEFINE(inst, sx128x_init, NULL, &sx128x_data_##node_id,                    \
+                          &sx128x_config_##node_id, POST_KERNEL,                              \
+                          CONFIG_LORA_SX128X_INIT_PRIORITY, NULL);
+
+DT_INST_FOREACH_STATUS_OKAY(SX128X_DEFINE)
