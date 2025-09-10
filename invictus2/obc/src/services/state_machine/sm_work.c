f2d57920d006e35d2cef32d24d87134898725a3c
AlexSMFerreira <alexandre.smferreira@gmail.com>
2025-09-10 23:21:46 +0100

    added missing work queue init

--- invictus2/obc/src/services/state_machine/sm_work.c
+++ invictus2/obc/src/services/state_machine/sm_work.c
@@ -8,9 +8,13 @@ static struct sm_object sm_obj;
 
 LOG_MODULE_DECLARE(state_machine_service);
 
-//* K_THREAD_STACK_DEFINE(sm_work_q_stack, CONFIG_sm_work_q_STACK); */
-K_THREAD_STACK_DEFINE(sm_work_q_stack, 1024); // FIXME: Make KConfig
+#define SM_WORK_Q_PRIO 5               // TODO: make KConfig
+K_THREAD_STACK_DEFINE(sm_work_q_stack, 1024); // TODO: Make KConfig
 static struct k_work_q sm_work_q;
+
+#define SM_THREAD_PRIO 5               // TODO: make KConfig
+K_THREAD_STACK_DEFINE(sm_stack, 1024); // TODO: make KConfig
+static struct k_thread sm_thread;
 
 static void command_work_handler(struct k_work *work);
 static void weight_work_handler(struct k_work *work);
@@ -87,10 +91,6 @@ static void pressure_work_handler(struct k_work *work)
     k_oops(); // FIXME:
 }
 
-#define SM_THREAD_PRIO 5               // TODO: make KConfig
-K_THREAD_STACK_DEFINE(sm_stack, 1024); // TODO: make KConfig
-static struct k_thread sm_thread;
-
 void sm_thread_entry(void *p1, void *p2, void *p3)
 {
     ARG_UNUSED(p1);
@@ -102,7 +102,9 @@ void sm_thread_entry(void *p1, void *p2, void *p3)
     while (true) {
         // TODO: handle zbus stuff
         // read cmd bus
+
         
+
         smf_run_state(SMF_CTX(&sm_obj));
 
         // TODO sleep only difference
@@ -118,6 +120,7 @@ bool state_machine_service_setup(void)
     // For example, initialize hardware, data models, or configuration.
     // Return true if setup is successful, false otherwise.
     sm_init(&sm_obj);
+    k_work_queue_init(&sm_work_q);
     return true;
 }
 
@@ -127,11 +130,17 @@ void state_machine_service_start(void)
     // Typically, this would initialize the state machine object and set its initial state.
     //LOG_WRN_ONCE("NOT IMPLEMENTED");
     //return;
+    k_work_queue_start(&sm_work_q, sm_work_q_stack,
+                   K_THREAD_STACK_SIZEOF(sm_work_q_stack), SM_WORK_Q_PRIO,
+                   NULL);
+
     const k_tid_t tid =
         k_thread_create(&sm_thread, sm_stack, K_THREAD_STACK_SIZEOF(sm_stack),
                         sm_thread_entry, NULL, NULL, NULL, SM_THREAD_PRIO, 0, K_NO_WAIT);
 
     k_thread_name_set(tid, "state_machine");
+
+    
 }
 
 
